6f0bf73ac25e4400dfe8888358060e31d9a54b9b
Jayashri Vadde <IN04065@azentio.com>
2025-12-07 16:43:45 +0530

    4th Dec

---
+++ LogicalPrograms/EX2Factorial.py
@@ -0,0 +1,10 @@
+num=4
+
+fact = 1      #1  update =2  update fact =6 update fact =25
+
+for i in range(1,num+1):   #1,2,3,4      5<5 false
+    fact=fact*i        #1=1*1     2=2*1     6=3*2   24=4*6
+
+
+    print(fact)
+ #4*3*2*1
---
+++ LogicalPrograms/EX31to100 even.py
@@ -0,0 +1,6 @@
+#Print Even  from 1 to 100
+
+num=100
+for i in range(1,num+1):   # 1<101
+    if i%2==0:
+        print(i)
