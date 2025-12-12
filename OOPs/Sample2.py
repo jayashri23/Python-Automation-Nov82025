1ec67f294a151ef8e6e6e047d6508576f12e7f2f
Jayashri Vadde <IN04065@azentio.com>
2025-12-12 20:37:08 +0530

    9 Dec

---
+++ OOPs/Sample2.py
@@ -0,0 +1,28 @@
+#Static method
+from stringprep import map_table_b3
+
+
+class Demo2:
+
+    @staticmethod            #static method
+    def m3():
+        print("Print static method m3")
+
+    @staticmethod
+    def m4():
+        print("Print static method m4")
+
+    #Nonstatic method
+    def m5(self):
+        print("Print Non static method m5")
+
+    #static method call
+Demo2().m3()
+Demo2().m4()
+
+
+    #Nonstatic method call
+d=Demo2()
+d.m5()
+
+
