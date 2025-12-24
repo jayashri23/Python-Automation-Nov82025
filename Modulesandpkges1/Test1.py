2f58f0cd5b5c760927e6d35fdc16ca46641d36f8
Jayashri Vadde <IN04065@azentio.com>
2025-12-24 16:12:57 +0530

    22 Dec

---
+++ Modulesandpkges1/Test1.py
@@ -0,0 +1,14 @@
+#Module1
+
+def f1():
+    print("Running function 1 function from test 1 module")
+
+def f2():
+    print("Running function 2 function from test 1 module")
+
+class MyClass:
+
+    def mm1(self):
+        print("Running mm 1 function from test 1 module")
+    def mm2(self):
+        print("Running mm 2 function from test 1 module")
---
+++ StringClass/Sample1.py
@@ -0,0 +1,22 @@
+#-------String-----------
+#string is data type and inbuilt class
+
+s1="Jayashri"
+print(len(s1))
+print(s1.upper())
+print(s1)
+s1=s1.upper()     #permenant changed into upper case called initialization
+print(s1)
+
+s2="VISHAL"
+print(len(s2))
+print(s2.lower())
+print(s2)
+
+s2=s2.lower()    #permenant changed into lowe case
+print(s2)
+
+s3="Sarika"
+print(s3.endswith("s"))   #if string end with given then true or false output comes
+
+print(s3.startswith("S"))
